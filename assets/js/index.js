e9c62a74da73b40547d1b0219c11a0d1d58ffbe1
04947419024 <04947419024@senacrs.edu.br>
2024-07-08 16:49:56 -0300

    subindo tela de cadastro dentro do html do login

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,21 @@
-const iconPassword = document.getElementById('view-password')
+const iconPassword = document.querySelectorAll('#view-password')
 
-iconPassword.addEventListener('click', () => {
-    const inputPassword = document.getElementById('password')
-    if (inputPassword.type == "password") {
-        inputPassword.type = "text"
-
-        iconPassword.classList.remove('bx-show')
-        iconPassword.classList.add('bx-hide')
-    } else {
-        inputPassword.type = "password"
-        iconPassword.classList.remove('bx-hide')
-        iconPassword.classList.add('bx-show')
-    }
-
+iconPassword.forEach(icon => {
+    icon.addEventListener('click', () => {
+        const inputPassword = icon.previousElementSibling
+        console.log(inputPassword);
+        if (inputPassword.type == "password") {
+            inputPassword.type = "text"
+        
+            icon.classList.remove('bx-show')
+            icon.classList.add('bx-hide')
+        } else {
+            inputPassword.type = "password"
+            icon.classList.remove('bx-hide')
+            icon.classList.add('bx-show')
+        }
+    
+    })
 })
+
+
