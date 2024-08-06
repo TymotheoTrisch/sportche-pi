0bf7cadbcfb7c56b6b416c62cbd5f81b40d92060
TymotheoTrisch <tymotheo.dev@gmail.com>
2024-08-05 23:58:38 -0300

    comitando partidas

---
+++ assets/js/search.js
@@ -0,0 +1,16 @@
+const iconsClose = document.querySelectorAll('.icon-close');
+const inputsSearch = document.querySelectorAll('#search')
+
+iconsClose.forEach(iconClose => {
+    iconClose.addEventListener('click', () => {
+        iconClose.previousElementSibling.value = '';
+        iconClose.style.display = 'none';
+    })    
+})
+
+inputsSearch.forEach(inputSearch => {
+    inputSearch.addEventListener('input', () => {
+        inputSearch.nextElementSibling.style.display = 'flex';
+    
+    })    
+})
